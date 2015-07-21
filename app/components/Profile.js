ad158183cd2ef08e262d0909f2b681db045db12d
Michael Updegraff <michael.updegraff@gmail.com>
2015-07-21 16:27:09 -0700

    Further refinements

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -5,23 +5,39 @@ var Repos = require('./Github/Repos');
 var Notes = require('./Notes/Notes');
 var ReactFireMixin = require('reactfire');
 var Firebase = require('firebase');
+var helpers = require('../utils/helpers')
 
 var Profile = React.createClass({
-   mixins: [Router.State, ReactFireMixin],
+    mixins: [Router.State, ReactFireMixin],
     getInitialState: function(){
        return {
            notes: [],
-           bio: {name: 'Michael'},
-           repos:[1,2,3]
+           bio: {},
+           repos:[]
        }
-   },
+    },
+    init: function(){
+      var childRef = this.ref.child(this.getParams().username);
+      this.bindAsArray(childRef, 'notes');
+
+      helpers.getGithubInfo(this.getParams().username)
+        .then(function(dataObj){
+          this.setState({
+            bio: dataObj.bio,
+            repos: dataObj.repos
+          });
+        }.bind(this));
+    },
     componentDidMount: function() {
         this.ref = new Firebase('https://github-note-taker-mu.firebaseio.com');
-        var childRef = this.ref.child(this.getParams().username);
-        this.bindAsArray(childRef, 'notes');
+        this.init();
     },
     componentWillUnmount: function(){
         this.unbind('notes');
+    },
+    componentWillReceiveProps: function(){
+        this.unbind('notes');
+        this.init();
     },
     handleAddNote: function(newNote) {
       this.ref.child(this.getParams().username).set(this.state.notes.concat(newNote));
